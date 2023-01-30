893ee5806f3ff95e5f63d6e84144e9c0efe900a7
Saurabh Hindlekar <saurabh.hindlekar@hotmail.com>
2023-01-29 16:59:40 -0800

    multiple posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Post from "./components/Post";
 
 function App() {
-  return <Post />; // A function pointer isn't it?
-  // The custom components must start with an upper case character
-  // Default components start with a lower case character
+  return (
+    <main>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </main> // Must return one root element - cannot return multiple elements
+  );
 }
 
 export default App;
