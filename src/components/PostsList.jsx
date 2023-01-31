2c70be23de5fb057dd553e73f83481ed1fdd5a50
Saurabh Hindlekar <saurabh.hindlekar@hotmail.com>
2023-01-30 19:23:07 -0800

    render content conditionally

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import classes from "./PostsList.module.css";
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
@@ -12,12 +17,24 @@ function PostsList() {
     setEnteredAuthor(event.target.value);
   }
 
+  let modalContent;
+
+  if (modalIsVisible) {
+    modalContent = (
+      <Modal onClose={hideModalHandler}>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        ></NewPost>
+      </Modal>
+    );
+  } else {
+    modalContent = false;
+  }
+
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      ></NewPost>
+      {modalContent}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Will" body="Some more comment" />
