b0df5949cc57335ab2b1f4fad245d186745cf379
Saurabh Hindlekar <saurabh.hindlekar@hotmail.com>
2023-02-05 12:15:36 -0800

    react router for form submission, leaner code

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import Posts, { loader as postsLoader } from "./routes/Posts.jsx";
 import RootLayout from "./routes/RootLayout.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import NewPost from "./routes/NewPost.jsx";
+import NewPost, { action as newPostAction } from "./routes/NewPost.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,7 +16,13 @@ const router = createBrowserRouter([
         element: <Posts></Posts>,
         loader: postsLoader, //Element is rendered only after this function is executed
         //This can be avoided use react-router features
-        children: [{ path: "/create-post", element: <NewPost></NewPost> }],
+        children: [
+          {
+            path: "/create-post",
+            element: <NewPost></NewPost>,
+            action: newPostAction,
+          },
+        ],
       },
     ],
   },
@@ -27,3 +33,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
+
