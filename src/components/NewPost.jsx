17ed2aefffacf915c96ca8181aae657b64693217
Saurabh Hindlekar <saurabh.hindlekar@hotmail.com>
2023-02-04 21:58:44 -0800

    getting new posts

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,20 +1,46 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost({ onBodyChange, onAuthorChange, onCancel }) {
+function NewPost({ onCancel, onAddPost }) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor,
+    };
+    onAddPost(postData);
+    onCancel();
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
         <textarea
           id="body"
           required
           rows={3}
-          onChange={onBodyChange}
+          onChange={bodyChangeHandler}
         ></textarea>
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onAuthorChange}></input>
+        <input
+          type="text"
+          id="name"
+          required
+          onChange={authorChangeHandler}
+        ></input>
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={onCancel}>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,29 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import classes from "./PostsList.module.css";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
+  // The following is something we should NOT do - causes an infinite loop
+  // fetch("http://localhost:8080/posts").then(response => response.json().then(data => {
+  //   setPosts(data.posts);
+  // }));
+  const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      // Add error handling here is response code is not 200
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+    fetchPosts();
+  }, []);
 
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
+  function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   let modalContent;
 
   if (isPosting) {
     modalContent = (
       <Modal onClose={onStopPosting}>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-          onCancel={onStopPosting}
-        ></NewPost>
+        <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}></NewPost>
       </Modal>
     );
   } else {
@@ -33,10 +47,24 @@ function PostsList({ isPosting, onStopPosting }) {
   return (
     <>
       {modalContent}
-      <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author="Will" body="Some more comment" />
-      </ul>
+      {!isFetching && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>No Posts!.</h2>
+          <p>Please add some.</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
+        </div>
+      )}
     </>
   );
 }
